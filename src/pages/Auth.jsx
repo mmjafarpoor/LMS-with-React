ccef23fa9cede60bae0e7fa7db970e8f21233c7c
ᗰᕼᗰᗪ ᗰᕼᗪI <jafarpoorm00@gmail.com>
2026-05-25 17:30:23 +0330

    login-page branch last

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import styles from "../styles/AuthContainer.module.css";
+import styles from "../styles/Auth.module.css";
 
 function AuthContainer() {
   return (
