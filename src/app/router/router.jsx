86ce6dcab370bc4bf3a3d1a5de7c128a39454373
ᗰᕼᗰᗪ ᗰᕼᗪI <jafarpoorm00@gmail.com>
2026-05-25 17:00:54 +0330

    mehdiiiiii

---
+++ src/app/router/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import AuthContainer from "../../pages/AuthContainer";
+import Login from "../../components/AuthContainer/Login/Login";
+import SignUp from "../../components/AuthContainer/SignUp/SignUp";
+import ForgetPassword from "../../components/AuthContainer/ForgetPassword/ForgetPassword";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <AuthContainer />,
+        children: [
+        { path: "/login", element: <Login /> },
+        { path: "/sign-up", element: <SignUp /> },
+        { path: "/forget-password", element: <ForgetPassword /> },
+        ],
+    },
+]);
+
+export default router;
