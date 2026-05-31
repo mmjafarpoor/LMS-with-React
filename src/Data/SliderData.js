c9485c9ed868b4565acf74e9702a1e756e8f3f6a
ᗰᕼᗰᗪ ᗰᕼᗪI <jafarpoorm00@gmail.com>
2026-06-01 00:14:49 +0330

    Home Slider First Attempt

---
+++ src/Data/SliderData.js
@@ -0,0 +1,20 @@
+const SliderData = [
+    {
+        id: 1,
+        name: "آموزش Node.js",
+        price: "190 هزار",
+        imageURL:"/images/offer_mask3.png",
+        likeCount:280,
+        dislikeCount: 250,
+        instructorName:"محسن",
+    },
+    {
+        id: 2,
+        name: "طراحی کامل گرافیک برای مبتدی ها ",
+        price: "180 هزار",
+        imageURL:"/images/offer_mask3.png",
+        likeCount:180,
+        dislikeCount: 20,
+        instructorName:"زهرا",
+    },]
+export default SliderData;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,36 @@ const Home = () => {
         </div>
       </div>
       <div className={Style.latestOnlineCourses}>
-        <div className={Style.latestOnlineCoursesHeading}></div>
+        <div className={Style.latestOnlineCoursesHeading}>
+          <div className={Style.latestOnlineCoursesTitleContainer}>
+            <img src="/images/blueLine.png" alt="Blue-Line" />
+            <span>جدیترین دوره های انلاین</span>
+          </div>
+          <div className={Style.latestOnlineCoursesDescription}>محبوب ترین دوره های آموزشی نویسندگان متخصص ما را بررسی کنید.</div>
+        </div>
+        <div className={Style.sliderContainer}>
+          <div className={Style.sliderGallery}>
+            <div className={Style.sliderItem}>
+              <div className={Style.sliderItemImageWrapper}>
+                <img src="/images/reactProductCardPic.png" alt="" className={Style.sliderItemImage}/>
+              </div>
+              <div className={Style.sliderItemMeta}>
+                <div className={Style.sliderItemMetaHeading}>
+                  <span className={Style.sliderItemTitle}>آموزش Node.js</span>
+                  <span className={Style.sliderItemDescription}>Node.js یک پلتفرم قدرتمند برای توسعهٔ برنامههای سرور با استفاده از جاوااسکریپت است. با استفاده از Node.js، میتوانید اپلیکیشنهای سریع و مقیاسپذیر بسازید. یادگیری آن آسان است، بهخصوص اگر با جاوااسکریپت آشنا باشید.</span>
+                </div>
+                <div className={Style.sliderItemPriceTagsContainer}>
+                  <div className={Style.sliderItemOlderPriceContainer}>
+                    <div className={Style.sliderItemOlderPrice}>40.000 تومان</div>
+                    <div className={Style.sliderItemPriceOfferLine}></div>
+                    <div className={Style.sliderItemPriceOfferPercentage}>10%</div>
+                  </div>
+                  <div className={Style.newPrice}>40.000 تومان</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
